cd1c9d36d34136cf7619a2284c9e05b62362938e
ASAD HAMEED <37589169+asadamatic@users.noreply.github.com>
2020-04-21 13:12:36 +0500

    Add files via upload

--- Data Structures/Linked Lists/Signly Linked List/singlyLL.py
+++ Data Structures/Linked Lists/Signly Linked List/singlyLL.py
@@ -1,4 +1,4 @@
-""" Creating a linked list and implenting print, append, prepend, insert_after_node and insert_before_node methods with it """
+""" Creating a linked list and implenting print, append, prepend, insert_after_node, insert_before_node, delete, delete_head and delete_last methods with it """
 
 class Node:
 
@@ -107,7 +107,7 @@ class LinkedList:
 
         previous_node = None
 
-        while last_node.next:
+        while last_node:
 
             if next_node is last_node:# Matching given node against all the nodes in the linked list one by one
 
@@ -127,15 +127,74 @@ class LinkedList:
             previous_node = last_node
             last_node = last_node.next
 
+    def delete_after_node(self, previous_node):
+
+        """ Delete the node that is after the given node """
+        if not previous_node.next: #If given node if null
+
+            print('There is no node after the given node :(')
+
+            return
+
+        last_node = self.head
+
+        while last_node:
+
+            if previous_node is last_node: #Matching previous node against linked list nodes
+
+                previous_node.next = last_node.next.next #Assigning the the node present after the deleted node
+
+                return
+
+            last_node = last_node.next
+
+
+    def delete_head(self):
+
+        """ Delete the head (first) node of the linked list """
+        self.head = self.head.next
+
+    def delete_last(self):
+
+        """ Delete the last node of the linked list """
+        last_node = self.head
+
+        while last_node.next.next:# Stopping the iterator on the 2nd last node
+
+            last_node = last_node.next
+
+        last_node.next = None #Deleting the reference of last node from 2nd last node
+
+    def delete(self, node):
+
+        """ Delete the given node in the linked list """
+        if not node:
+
+            print('Given node does not exist :(')
+
+            return
+
+        last_node = self.head
+        previous_node = None #Keeping a trace of the node present before the node to be deleted
+
+        while last_node:
+
+            if node is last_node:
+
+                if previous_node is None: #Check if the node to be deleted is the head of the linked list
+
+                    self.delete_head()
+
+                    return
+
+                else:
+
+                    previous_node.next = last_node.next
+
+                    return
             
-        if next_node is last_node:
-
-            self.append(data)
-
-
-
-
-
+            previous_node = last_node
+            last_node = last_node.next     
 
 linked_list = LinkedList()
 linked_list.append("1")
@@ -146,4 +205,21 @@ linked_list.insert_before_node(linked_list.head, "4")
 linked_list.insert_before_node(linked_list.head.next, "5")
 linked_list.insert_before_node(linked_list.head.next.next.next.next.next, "11")
 linked_list.print()
-
+print('...')
+linked_list.delete_head()
+linked_list.print()
+print('...')
+linked_list.delete_last()
+linked_list.print()
+print('...')
+linked_list.delete(linked_list.head.next.next.next.next)
+linked_list.print()
+print('...')
+linked_list.insert_before_node(linked_list.head.next.next.next, "100")
+linked_list.print()
+print('...')
+linked_list.delete(linked_list.head.next.next.next.next)
+linked_list.print()
+print('...')
+linked_list.delete_after_node(linked_list.head.next.next.next)
+linked_list.print()
